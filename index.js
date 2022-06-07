646cc05b497fbc01849e2244edc312059de29c82
lotus <daniel.younghwan.lee@gmail.com>
2022-06-07 19:08:33 -0400

    rearranging code for writefile and finishing engineer prompt function

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-const managerArray = [];
 const employeeArray = [];
 
 const promptManager = () => {
@@ -35,70 +34,102 @@ const promptManager = () => {
         ])
         .then(managerResponse => {
             const manager = new Manager(managerResponse.name, managerResponse.id, managerResponse.email, managerResponse.officeNumber);
-            managerArray.push(manager);
-            return managerArray;
-        }).then(managerArray => {
-            return generatePage(managerArray);
+            employeeArray.push(manager);
+            promptEmployees(employeeArray)
         })
-        .then(pageHTML => {
-            return writeFile(pageHTML);
-        })
-        .catch(err => {
-            console.log(err);
-        });
 }
 
-const promptEmployees = () => {
+const promptEmployees = (employeeArray) => {
     return inquirer
         .prompt([
             {
                 type: 'list',
                 name: 'role',
                 message: 'Would you like to add a new employee?',
-                choices: ['Engineer', 'Intern', 'finish'],
-                validate: choices => {
-                    if (choices === finish) {
-                    return false;
-                    }
-                }
-            },
+                choices: ['Engineer', 'Intern', 'finish']
+            }
+        ])
+        .then(choiceResponse => {
+            let { role } = choiceResponse;
+            if (role === 'finish') {
+                console.log(employeeArray)
+
+                let pageHTML = generatePage(employeeArray);
+                writeFile(pageHTML)
+            } else if (role === 'Engineer') {
+                promptEngineer(employeeArray);
+            }
+        });
+}
+
+const promptEngineer = (employeeArray) => {
+    console.log(employeeArray)
+    return inquirer
+        .prompt([
             {
                 type: 'input',
                 name: 'name',
-                message: "What is the employee's name?"
+                message: "What is the engineer's name?"
             },
             {
                 type: 'input',
                 name: 'id',
-                message: "What is the employee's ID?"
+                message: "What is the engineer's ID?"
             },
             {
                 type: 'input',
                 name: 'email',
-                message: "What is the employee's email address?"
+                message: "What is the engineer's email address?"
             },
             {
                 type: 'input',
                 name: 'github',
-                message: "What is the engineer's GitHub username?",
-                when: (response) => response.role === 'Engineer',
-            },
-            {
-                type: 'input',
-                name: 'school',
-                message: "what school does the intern attend?",
-                when: (response) => response.role === 'Intern',
+                message: "What is the engineer's GitHub username?"
             }
         ])
-        .then(employeeResponse => {
-            console.log(employeeResponse);
-            if (employeeResponse.role === 'Engineer') {
-                const engineer = new Engineer(employeeResponse.name, employeeResponse.id, employeeResponse.email, employeeResponse.github);
-                employeeArray.push(engineer);
-                console.log(employeeArray);
-            }
-        })
+        .then(engineerResponse => {
+            const engineer = new Engineer(engineerResponse.name, engineerResponse.id, engineerResponse.email, engineerResponse.github);
+            employeeArray.push(engineer);
+            promptEmployees(employeeArray)
+        });
 }
+//             {
+//                 type: 'input',
+//                 name: 'name',
+//                 message: "What is the employee's name?"
+//             },
+//             {
+//                 type: 'input',
+//                 name: 'id',
+//                 message: "What is the employee's ID?"
+//             },
+//             {
+//                 type: 'input',
+//                 name: 'email',
+//                 message: "What is the employee's email address?"
+//             },
+//             {
+//                 type: 'input',
+//                 name: 'github',
+//                 message: "What is the engineer's GitHub username?",
+//                 when: (response) => response.role === 'Engineer',
+//             },
+//             {
+//                 type: 'input',
+//                 name: 'school',
+//                 message: "what school does the intern attend?",
+//                 when: (response) => response.role === 'Intern',
+//             }
+//         ])
+//         .then(employeeResponse => {
+//             console.log(employeeResponse);
+//             if (employeeResponse.role === 'Engineer') {
+//                 const engineer = new Engineer(employeeResponse.name, employeeResponse.id, employeeResponse.email, employeeResponse.github);
+//                 employeeArray.push(engineer);
+//                 console.log(employeeArray);
+//             }
+//         })
+// }
 
 const writeFile = response => {
     return new Promise((resolve, reject) => {
@@ -117,4 +148,4 @@ const writeFile = response => {
 };
 
 promptManager()
-    .then(promptEmployees)
+    // .then(promptEmployees)
--- src/page-template.js
+++ src/page-template.js
@@ -1,22 +1,22 @@
-const generateManager = function (managerArray) {
+const generateManager = function (employeeArray) {
     return `
     <div class="card has-text-centered">
         <div class="card-header is-flex is-justify-content-center is-flex-direction-column py-2 has-background-warning">
             <h1 class="is-size-4 has-text-weight-semibold">
-            ${managerArray[0].name} </h1>
+            ${employeeArray[0].name} </h1>
             <h2 class="is-size-5">
             Manager </h2>
         </div>
         <div class="card-content">
-            ID: ${managerArray[0].id} <br>
-            Email: <a href="mailto:${managerArray[0].email}">${managerArray[0].email}</a><br>
-            Office number: ${managerArray[0].officeNumber}
+            ID: ${employeeArray[0].id} <br>
+            Email: <a href="mailto:${employeeArray[0].email}">${employeeArray[0].email}</a><br>
+            Office number: ${employeeArray[0].officeNumber}
         </div>
     </div>
     `;
 }
 
-const generatePage = function (managerArray) {
+const generatePage = function (employeeArray) {
     return `
     <!DOCTYPE html>
     <html lang="en" class="has-background-light">
@@ -37,8 +37,10 @@ const generatePage = function (managerArray) {
             </p>
         </nav>
         <section class="section my-6 is-flex is-justify-content-center">
-            ${generateManager(managerArray)}
+            ${generateManager(employeeArray)}
         </section>
+        <footer class="footer has-background-primary">
+        </footer>
     </body>
     </html>
     `
